6d0907ed8e4edb51ef9cc3a97071df63048bbc1c
Adiuyaniv <86191221+Adiuyaniv@users.noreply.github.com>
2023-01-22 10:55:08 +0200

    Add files via upload

---
+++ importfile_modify2.m
@@ -0,0 +1,16 @@
+function modified_data = importfile_modify2(file_path, col_index, new_vector)
+
+% Import data from text file
+data = importfile(file_path);
+
+% Replace the vector in the specified column index with the new vector, starting from the 10th row
+data(10:end,col_index) = new_vector;
+
+% Save the modified data to a new file
+[pathstr,name,ext] = fileparts(file_path);
+new_file_name = [name, '_modified', ext];
+new_file_path = fullfile(pathstr, new_file_name);
+save(new_file_path,'data');
+
+modified_data = data;
+end
